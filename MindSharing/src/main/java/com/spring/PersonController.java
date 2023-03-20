841d9622ae1ccd0cc25471be2f0cadc71ca50352
이진아 <mnb2098@naver.com>
2023-03-20 14:16:15 +0900

    스프링 파일 추가 후 간단한 실행테스트
    http://localhost:8080/myinfo
    결과 확인 후 커밋

---
+++ MindSharing/src/main/java/com/spring/PersonController.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @GetMapping("/myinfo")
+    public Person getPerson() {
+        Person person = new Person();
+        person.setName("ㅇㅣ진아");
+        person.setAge(22);
+        person.setAddress("경기");
+        person.setJob("대학생");
+        return person;
+    }
+}
