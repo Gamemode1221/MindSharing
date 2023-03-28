9a262eba8db3c2ed5777189912aa060ee546c70c
Gamemode1221 <yongho1221@gmail.com>
2023-03-28 16:31:25 +0900

    User -> Users 변경

--- MindSharing/src/main/java/com/spring/Repository/UserRepository.java
+++ MindSharing/src/main/java/com/spring/Repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.spring.Repository;
 
-import com.spring.entity.User;
+import com.spring.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 }
--- MindSharing/src/main/java/com/spring/Service/UserService.java
+++ MindSharing/src/main/java/com/spring/Service/UserService.java
@@ -1,7 +1,7 @@
 package com.spring.Service;
 
 import com.spring.Repository.UserRepository;
-import com.spring.entity.User;
+import com.spring.entity.Users;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,9 +15,9 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder; //빈으로 객체를 받아오게끔
 
-    public User create(String username, String password) {
-        User user = new User();
-        user.setUsername(username);
+    public Users create(String username, String password) {
+        Users user = new Users();
+        user.setNickname(username);
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); //시큐리티의 암호화
         user.setPassword(passwordEncoder.encode(password));
         this.userRepository.save(user);
