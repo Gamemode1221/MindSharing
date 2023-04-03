a19bcdb92d0bf5882bc12154de49e4d2cfba2a58
Gamemode1221 <yongho1221@gmail.com>
2023-04-03 16:33:57 +0900

    APItest용 react 파일

---
+++ MindSharing/src/main/resources/templates/InputForm.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function InputForm() {
+    const [text1, setText1] = useState('');
+    const [text2, setText2] = useState('');
+    const [text3, setText3] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('/api/test', {text1, text2, text3})
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Text1:
+                <input type="text" value={text1} onChange={(e) => setText1(e.target.value)} />
+            </label>
+            <label>
+                Text2:
+                <input type="text" value={text2} onChange={(e) => setText2(e.target.value)} />
+            </label>
+            <label>
+                Text3:
+                <input type="text" value={text3} onChange={(e) => setText3(e.target.value)} />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
+    );
+}
