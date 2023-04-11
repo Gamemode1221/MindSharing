1a03bf8b2e6464ace7f911abbaedd92bf3406358
Gamemode1221 <yongho1221@gmail.com>
2023-04-11 11:29:32 +0900

    추가 수정

--- MindSharing/src/main/java/com/spring/Service/UserSecurityService.java
+++ MindSharing/src/main/java/com/spring/Service/UserSecurityService.java
@@ -1,7 +1,6 @@
 package com.spring.Service;
 
 import com.spring.Repository.UserRepository;
-import com.spring.entity.Test;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,11 +22,11 @@ public class UserSecurityService implements UserDetailsService {
 
     @Override // loadUserByUsername 메서드에 의해 리턴된 User 객체(리턴)의 비밀번호가 화면으로부터 입력 받은 비밀번호와 일치하는지를 검사하는 로직
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Optional<Test> test1 = this.userRepository.findByname(username);
-        if (test1.isEmpty()) {
+        Optional<com.spring.entity.User> userRepo = this.userRepository.findByName(username);
+        if (userRepo.isEmpty()) {
             throw new UsernameNotFoundException("사용자를 찾을수 없음요.");
         }
-        Test test = test1.get();
+        com.spring.entity.User user = userRepo.get();
         List<GrantedAuthority> authorities = new ArrayList<>();
 
         if ("admin".equals(username)) { //name이 admin이면 ADMIN 권한 부여 아니면 USER 권한 부여
@@ -37,6 +36,6 @@ public class UserSecurityService implements UserDetailsService {
         }
 
         //name, password, 권한을 입력으로 스프링 시큐리티의 User 객체를 생성하여 리턴
-        return new User(test.getName(), test.getPassword(), authorities);
+        return new User(user.getUserId(), user.getPassword(), authorities);
     }
 }
