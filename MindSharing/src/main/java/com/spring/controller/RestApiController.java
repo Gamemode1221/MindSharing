b0f78e4d947ae3d81af4296d0a7b5254b57fac0a
Gamemode1221 <yongho1221@gmail.com>
2023-04-11 14:07:57 +0900

    프론트 데이터 연동 예제 생성

--- MindSharing/src/main/java/com/spring/controller/RestApiController.java
+++ MindSharing/src/main/java/com/spring/controller/RestApiController.java
@@ -3,9 +3,12 @@ package com.spring.controller;
 import com.spring.Service.ApiService;
 import com.spring.entity.ApiTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
@@ -13,11 +16,6 @@ public class RestApiController {
 
     @Autowired
     private ApiService apiService;
-
-//    @GetMapping("/test")
-//    public String test() {
-//        return "test page";
-//    }
 
     @PostMapping
     public void addTests(@RequestBody ApiTest apiTest) {
@@ -30,10 +28,13 @@ public class RestApiController {
         return apiService.getTest();
     }
 
-//    @GetMapping("/test/list")
-//    @ResponseBody
-//    public String testList() {
-//        return apiService.getTest().toString();
-//    }
+    // api/message 매핑으로 프론트에게 데이터를 보냄
+    @GetMapping("/api/message")
+    public ResponseEntity<Map<String, String>> getMessage() {
+        Map<String, String> message = new HashMap<>();
+        message.put("message", "Hello from Spring Boot!");
+
+        return ResponseEntity.ok(message);
+    }
 
 }
