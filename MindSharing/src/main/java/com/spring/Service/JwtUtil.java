0786ce70abbbac9f0b477f6ec64ce37117af2d63
Gamemode1221 <yongho1221@gmail.com>
2023-04-12 16:11:18 +0900

    JwtUtil 클래스 내용 추가

--- MindSharing/src/main/java/com/spring/Service/JwtUtil.java
+++ MindSharing/src/main/java/com/spring/Service/JwtUtil.java
@@ -1,8 +1,16 @@
 package com.spring.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtUtil {
@@ -15,22 +23,43 @@ public class JwtUtil {
 
     // JWT 생성
     public String generateToken(UserDetails userDetails) {
-        // ... 코드 작성
+        Map<String, Object> claims = new HashMap<>();
 
-        return null;
-    }
-
-    // JWT 유효성 검사
-    public boolean validateToken(String token, UserDetails userDetails) {
-        // ... 코드 작성
-
-        return false;
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
     }
 
     // JWT에서 사용자 정보 추출
     public String getUsernameFromToken(String token) {
-        // ... 코드 작성
+        return getClaimFromToken(token, Claims::getSubject);
+    }
 
-        return null;
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    // JWT 유효성 검사
+    public boolean validateToken(String token, UserDetails userDetails) {
+        String username = getUsernameFromToken(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
     }
 }
