be84a2bf0903c560ad1f8d5f2579d02f357bb4a3
이진아 <mnb2098@naver.com>
2023-04-19 14:56:31 +0900

    팀 저장 서비스

---
+++ MindSharing/src/main/java/com/spring/Service/TeamService.java
@@ -0,0 +1,36 @@
+package com.spring.Service;
+
+
+import com.spring.Repository.TeamRepository;
+import com.spring.entity.Team;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TeamService {
+
+    private final TeamRepository teamRepository;
+
+    public TeamService(TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    public Team create(String teamName, Long ownerId, List<String> memberIds, Long mapId, String notice, List<String> favoriteUsers) {
+        Team team = new Team();
+        team.setTeamName(teamName); //팀 이름
+        team.setOwnerId(ownerId); //팀장 userid
+        team.setMemberId(String.join(",", memberIds)); //팀원 userid
+        team.setMapId(mapId); //보유 마인드맵 mapid
+        team.setNotice(notice); // 공지
+        team.setFavoriteUsers(String.join(",", favoriteUsers)); //즐겨찾기를 한 팀원 userid
+        this.teamRepository.save(team);
+        return team;
+    }
+
+    public List<Team> getTeam() {
+        return this.teamRepository.findAll();
+    }
+}
+
+
