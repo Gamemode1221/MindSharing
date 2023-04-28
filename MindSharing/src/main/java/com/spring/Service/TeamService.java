95e8ce973881afad53a21a585900d0e203e2667e
이진아 <mnb2098@naver.com>
2023-04-28 12:47:40 +0900

    TeamService에 team 메서드 추가

--- MindSharing/src/main/java/com/spring/Service/TeamService.java
+++ MindSharing/src/main/java/com/spring/Service/TeamService.java
@@ -3,6 +3,7 @@ package com.spring.Service;
 
 import com.spring.Repository.TeamRepository;
 import com.spring.entity.Team;
+import com.spring.entity.dto.TeamRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,28 +22,33 @@ public class TeamService {
         this.teamRepository = teamRepository;
     }
 
-    public void team(String teamName) {
-        Team team = new Team();
+    public boolean team(TeamRequest request) throws Exception {
+        try {
+            Team team = Team.builder()
+                    .teamName(request.getTeamname())
+                    .build();
 
-        team.setTeamName(teamName);
-        teamRepository.save(team);
+            teamRepository.save(team);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new Exception("잘못된 요청입니다.");
+        }
+        return true;
     }
 
-    public Team create(String teamName) { //, Long ownerId, List<String> memberIds, Long mapId, String notice, List<String> favoriteUsers
-        Team team = new Team();
-        team.setTeamName(teamName); //팀 이름
-
-
-//        team.setOwnerId(ownerId); //팀장 userid
-//        team.setMemberId(String.join(",", memberIds)); //팀원 userid
-//        team.setMapId(mapId); //보유 마인드맵 mapid
-//        team.setNotice(notice); // 공지
-//        team.setFavoriteUsers(String.join(",", favoriteUsers)); //즐겨찾기를 한 팀원 userid
-
-
-        this.teamRepository.save(team);
-        return team;
-    }
+//    public Team create(String teamName) { //, Long ownerId, List<String> memberIds, Long mapId, String notice, List<String> favoriteUsers
+//        Team team = new Team();
+//        team.setTeamName(teamName); //팀 이름
+////        team.setOwnerId(ownerId); //팀장 userid
+////        team.setMemberId(String.join(",", memberIds)); //팀원 userid
+////        team.setMapId(mapId); //보유 마인드맵 mapid
+////        team.setNotice(notice); // 공지
+////        team.setFavoriteUsers(String.join(",", favoriteUsers)); //즐겨찾기를 한 팀원 userid
+//
+//
+//        this.teamRepository.save(team);
+//        return team;
+//    }
 
 }
 
--- MindSharing/src/main/java/com/spring/controller/TeamController.java
+++ MindSharing/src/main/java/com/spring/controller/TeamController.java
@@ -1,17 +1,14 @@
 package com.spring.controller;
 
-import com.spring.Form.TeamCreateForm;
 import com.spring.Repository.TeamRepository;
 import com.spring.Service.TeamService;
-import com.spring.entity.Team;
-import lombok.Getter;
+import com.spring.entity.dto.TeamRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,39 +19,30 @@ public class TeamController {
     private final TeamRepository teamRepository;
 
 
-//    @PostMapping("/team")
-//    public ResponseEntity<Boolean> team(@RequestBody TeamRequest request) throws Exception {
-//        return new ResponseEntity<>(teamService.team(request), HttpStatus.OK);
+    @PostMapping("/team")
+    public ResponseEntity<Boolean> team(@RequestBody TeamRequest request) throws Exception {
+        return new ResponseEntity<>(teamService.team(request), HttpStatus.OK);
+    }
+
+
+//    @GetMapping("/teamform")
+//    public String team(TeamCreateForm teamCreateForm, Model model) {
+//        model.addAttribute("teamCreateForm", teamCreateForm);
+//        return "team_form";
 //    }
-
-
-    @GetMapping("/teamform")
-    public String team(TeamCreateForm teamCreateForm, Model model) {
-        model.addAttribute("teamCreateForm", teamCreateForm);
-        return "team_form";
-    }
-
-    @PostMapping("/teamform")
-    @ResponseBody
-    public String createTeam(@RequestParam String teamName, @ModelAttribute TeamCreateForm teamCreateForm, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "team_form";
-        }
-        teamService.create(teamName);
-        return teamName;
-    }
-
-    @Getter
-    @Setter
-    public static class TeamRequest {
-        private String teamName;
-    }
-
-
-    @GetMapping("/team_View.html")
-    public String teamView(Model model) {
-        List<Team> teams = teamRepository.findAll();
-        model.addAttribute("teams", teams);
-        return "team_View";
-    }
+//    @PostMapping("/teamform")
+//    @ResponseBody
+//    public String createTeam(@RequestParam String teamName, @ModelAttribute TeamCreateForm teamCreateForm, BindingResult bindingResult) {
+//        if (bindingResult.hasErrors()) {
+//            return "team_form";
+//        }
+//        teamService.create(teamName);
+//        return teamName;
+//    }
+//    @GetMapping("/team_View.html")
+//    public String teamView(Model model) {
+//        List<Team> teams = teamRepository.findAll();
+//        model.addAttribute("teams", teams);
+//        return "team_View";
+//    }
 }
