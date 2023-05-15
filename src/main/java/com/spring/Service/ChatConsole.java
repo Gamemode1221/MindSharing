a3ca87fd8d758aa8a387e9ccc9bdbff1418c3f7f
이진아 <mnb2098@naver.com>
2023-05-15 11:21:03 +0900

    console창에서 테스트 코드

--- src/main/java/com/spring/Service/ChatConsole.java
+++ src/main/java/com/spring/Service/ChatConsole.java
@@ -1,39 +1,48 @@
-//package com.spring.Service;
-//
-//
-//public class ChatConsole {
-//
-//    private final ChatGptService chatGptService;
-//
-//    public ChatConsole(ChatGptService chatGptService) {
-//        this.chatGptService = chatGptService;
-//    }
-//
-//    public void start() {
-//        System.out.println("Chat with GPT-3: (Type 'exit' to quit)");
-//        Scanner scanner = new Scanner(System.in);
-//        while (true) {
-//            System.out.print("You: ");
-//            String input = scanner.nextLine().trim();
-//            if (input.equals("exit")) {
-//                break;
-//            }
-//            ChatGptRequestDto requestDto = ChatGptRequestDto.builder()
-//                    .model(ChatGptConfig.MODEL)
-//                    .prompt(input)
-//                    .maxTokens(ChatGptConfig.MAX_TOKEN)
-//                    .temperature(ChatGptConfig.TEMPERATURE)
-//                    .topP(ChatGptConfig.TOP_P)
-//                    .build();
-//            HttpEntity<ChatGptRequestDto> requestEntity = chatGptService.buildHttpEntity(requestDto);
-//            ChatGptResponseDto responseDto = chatGptService.getResponse(requestEntity);
-//            List<Choice> choices = responseDto.getChoices();
-//            if (!choices.isEmpty()) {
-//                Choice choice = choices.get(0);
-//                String text = choice.getText();
-//                System.out.println("Bot: " + text);
-//            }
-//        }
-//        scanner.close();
-//    }
-//}
+package com.spring.Service;
+
+
+import com.spring.config.ChatGptConfig;
+import com.spring.entity.dto.ChatGptRequestDto;
+import com.spring.entity.dto.ChatGptResponseDto;
+import com.spring.entity.dto.Choice;
+import org.springframework.http.HttpEntity;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class ChatConsole {
+
+    private final ChatGptService chatGptService;
+
+    public ChatConsole(ChatGptService chatGptService) {
+        this.chatGptService = chatGptService;
+    }
+
+    public void start() {
+        System.out.println("Chat with GPT-3: (Type 'exit' to quit)");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("You: ");
+            String input = scanner.nextLine().trim();
+            if (input.equals("exit")) {
+                break;
+            }
+            ChatGptRequestDto requestDto = ChatGptRequestDto.builder()
+                    .model(ChatGptConfig.MODEL)
+                    .prompt(input)
+                    .maxTokens(ChatGptConfig.MAX_TOKEN)
+                    .temperature(ChatGptConfig.TEMPERATURE)
+                    .topP(ChatGptConfig.TOP_P)
+                    .build();
+            HttpEntity<ChatGptRequestDto> requestEntity = chatGptService.buildHttpEntity(requestDto);
+            ChatGptResponseDto responseDto = chatGptService.getResponse(requestEntity);
+            List<Choice> choices = responseDto.getChoices();
+            if (!choices.isEmpty()) {
+                Choice choice = choices.get(0);
+                String text = choice.getText();
+                System.out.println("Bot: " + text);
+            }
+        }
+        scanner.close();
+    }
+}
--- src/main/java/com/spring/config/ChatGptConfig.java
+++ src/main/java/com/spring/config/ChatGptConfig.java
@@ -3,7 +3,7 @@ package com.spring.config;
 public class ChatGptConfig {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
-    public static final String API_KEY = "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";
+    public static final String API_KEY = "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";
     public static final String MODEL = "text-davinci-003";
     public static final Integer MAX_TOKEN = 300;
     public static final Double TEMPERATURE = 0.0;
