5549a3f6f7cd8232e034fa71b6463042a2bd937c
이진아 <mnb2098@naver.com>
2023-05-15 14:09:58 +0900

    test commit

--- src/main/java/com/spring/config/ChatGptConfig.java
+++ src/main/java/com/spring/config/ChatGptConfig.java
@@ -11,3 +11,4 @@ public class ChatGptConfig {
     public static final String MEDIA_TYPE = "application/json; charset=UTF-8";
     public static final String URL = "https://api.openai.com/v1/completions";
 }
+//asd
